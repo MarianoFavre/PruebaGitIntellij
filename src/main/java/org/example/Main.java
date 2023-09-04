fc8311f5b1966ae448211294ebb3a415ba360413
Mariano Favre <marianofavre79@gmail.com>
2023-09-04 10:33:24 -0300

    Se creó la clase Persona.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Mensaje msj = new Mensaje();
         String nombre = msj.solicitaDatos("Ingrese tu nombre: ");
-        msj.saludar(nombre);
+        String apellidos = msj.solicitaDatos("Ingrese tus apellidos: ");
+        Persona p = new Persona(nombre, apellidos);
+        msj.saludar(p);
     }
 }
