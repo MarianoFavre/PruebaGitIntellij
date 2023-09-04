dfca02efce0974d6548301925a1e6d2b2f1df3f8
Mariano Favre <marianofavre79@gmail.com>
2023-09-04 10:40:06 -0300

    Se creó la clase RegistroPersona.

---
+++ src/main/java/org/example/RegistroPersona.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class RegistroPersona {
+    private String nombre, apellidos;
+
+    public RegistroPersona(String nombre, String apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+
+}
+
